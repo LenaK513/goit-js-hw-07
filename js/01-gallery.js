7bbdb40e1533a2d9ac09c8d9df7437d4084a281d
Olena <kryvtsovaolena@gmail.com>
2022-08-16 19:57:35 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
-console.log(gallery);
+// console.log(gallery);
 
 const listOfElements = (galleryItem) => {
   return `<li> <img class="image-item" src=${galleryItem.original} alt='${galleryItem.description}' /> </li>`;
