b19cb4a711dbfd3c3b942b227e43ef2ce05abb3f
Olena <kryvtsovaolena@gmail.com>
2022-08-16 21:08:00 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
-
-gallery.addEventListener("click", onBigUrlImageClick);
-
-function onBigUrlImageClick(event) {
-  // preventDefault();
-  console.log(event.target);
-}
-
 const listOfElements = (galleryItem) => {
   return `
 <div class="gallery__item">
@@ -24,7 +15,15 @@ const listOfElements = (galleryItem) => {
 </div>
 `;
 };
-
 const listOfImages = galleryItems.map(listOfElements).join("");
 
+const gallery = document.querySelector(".gallery");
+
 gallery.insertAdjacentHTML("beforeend", listOfImages);
+
+gallery.addEventListener("click", onBigUrlImageClick);
+
+function onBigUrlImageClick(event) {
+  // preventDefault();
+  console.log(event.target);
+}
