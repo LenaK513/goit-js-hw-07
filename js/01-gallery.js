6fb0ad5fb5c391f323cc3b12d819c43fe8974802
Olena <kryvtsovaolena@gmail.com>
2022-08-16 19:53:44 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,8 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
 const gallery = document.querySelector(".gallery");
+console.log(gallery);
 
 const listOfElements = (galleryItem) => {
   return `<li> <img class="image-item" src=${galleryItem.original} alt='${image.description}' /> </li>`;
@@ -10,4 +12,4 @@ const listOfImages = images.map(listOfElements).join("");
 
 gallery.insertAdjacentHTML("beforeend", listOfImages);
 console.log(gallery);
-console.log(galleryItems);
+// console.log(galleryItems);
