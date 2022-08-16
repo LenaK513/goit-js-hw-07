2c1240863c9f89d999f3295e33f670dc244dfee0
Olena <kryvtsovaolena@gmail.com>
2022-08-16 20:54:30 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,11 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
-// console.log(gallery);
-gallery.insertAdjacentHTML("beforeend", listOfImages);
-
-gallery.addEventListener("click", onBigUrlImageClick);
-
-function onBigUrlImageClick(event) {
-  preventDefault();
-  console.log(event.target);
-}
-
 const listOfElements = (galleryItem) => {
   // return `<li> <img class="image-item" src=${galleryItem.original} alt='${galleryItem.description}' /> </li>`;
   return `
 <div class="gallery__item">
-<a class="gallery__link" href="large-image.jpg">
+<a class="gallery__link" href="${galleryItem.original}">
   <img
     class="gallery__image"
     src="${galleryItem.preview}"
@@ -30,8 +19,13 @@ const listOfElements = (galleryItem) => {
 
 // console.log(listOfElements);
 const listOfImages = galleryItems.map(listOfElements).join("");
-
-// console.log(listOfImages);
-
+const gallery = document.querySelector(".gallery");
 // console.log(gallery);
-// console.log(galleryItems);
+gallery.insertAdjacentHTML("beforeend", listOfImages);
+
+gallery.addEventListener("click", onBigUrlImageClick);
+
+function onBigUrlImageClick(event) {
+  preventDefault();
+  console.log(event.target);
+}
