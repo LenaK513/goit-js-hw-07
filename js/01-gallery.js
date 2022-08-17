4aad1007cab22506f1c154f818c7ece93330a5a4
Olena <kryvtsovaolena@gmail.com>
2022-08-18 00:01:40 +0300

    edition

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -27,24 +27,8 @@ gallery.addEventListener("click", onLightboxOpenBigImg);
 const imgEl = document.querySelector("#img");
 
 function onLightboxOpenBigImg(event) {
-  // event.preventDefault();
+  event.preventDefault();
   lightbox.classList.add("active");
   imgEl.src = event.target.dataset.source;
-  console.log(event.target.dataset);
+  // console.log(event.target.dataset);
 }
-
-// const lightbox = document.createElement("div");
-// lightbox.id = "lightbox";
-// document.body.appendChild(lightbox);
-
-// galleryItems.forEach((galleryItem) => {
-//   galleryItem.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     lightbox.classList.add("active");
-//     const imgEl = document.createElement("img");
-//     imgEl.scr = galleryItem.original;
-//     lightbox.appendChild(img);
-
-//     // console.log(event.target.dataset);
-//   });
-// });
