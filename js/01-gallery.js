44b01714e945254c426a702a362bd9a25b4e5fc2
Olena <kryvtsovaolena@gmail.com>
2022-08-19 23:23:18 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -27,5 +27,5 @@ gallery.addEventListener("click", onBigUrlImageClick);
 function onBigUrlImageClick(event) {
   event.preventDefault();
 
-  console.dir(event.path.target);
+  console.dir(event);
 }
