68c7efe74f66057cff030e08e704b2af5bbf1d91
Olena <kryvtsovaolena@gmail.com>
2022-08-20 17:31:03 +0300

    add js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const listOfElements = (galleryItem) => {
+  return `
+<div class="gallery__item">
+<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>
+</div>
+`;
+};
 
-console.log(galleryItems);
+const listOfImages = galleryItems.map(listOfElements).join("");
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("beforeend", listOfImages);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: "250",
+});
